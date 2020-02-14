d4658b9f6ebde3ec4d18d7a92a41ef5e19be7272
Artur SKOBNIKOV (t422056) <arskobnikov-ext@airfrance.fr>
2020-02-14 17:48:39 +0000

    feat(id3): create 2 empty functions for computing entropy and information gain

---
+++ id3/id3.py
@@ -0,0 +1,25 @@
+
+
+
+def get_entropy(data_set):
+    """
+    Calculate and return the entropy of the given data set.
+    Entropy measures how informative or rich in diversity is a data set.
+    Higher is entropy, more heterogeneous the data set is and vice versa.
+
+    :param data_set:
+    :return: int entropy (>=0).
+    """
+    # TODO
+    return
+
+def get_information_gain(data_set, attribute):
+    """
+    Calculate and return the information gain obtained by splitting the data set on the given attribute.
+    Information gain = (entropy (information richness) of the given data set) - (total sum of entropies of all subsets
+    obtained by splitting the given data set on the given attribute).
+    :return:
+    """
+    #TODO
+    return
+
