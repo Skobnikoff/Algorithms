a5cd138454c54b50e45ef6e3fe8d9866331d9401
Skobnikoff <arthur.skobnikov@gmail.com>
2020-02-16 21:21:38 +0100

    feat(id3): compute the entropy of the split data set

--- id3/id3.py
+++ id3/id3.py
@@ -29,3 +29,22 @@ def get_information_gain(data_set, attribute):
     #TODO
     return
 
+
+def get_post_split_entropy(data_set, attr, target_col='class'):
+    """
+    Calculate and return the entropy of the given data set after it is split on the given attribute.
+    This entropy measures how informative or rich in diversity the data set becomes after the split.
+    It should be smaller then the entropy of the data set before the split.
+
+    :param data_set: pandas.DataFrame. Contains the training data 'X' AND the classification target 'y'.
+    :param attr: str. The name of the attribute (data frame column name) to split the data frame on.
+    :param target_col: str. The name of a column that contains the classification target 'y'.
+    :return: int entropy (>=0).
+    """
+    entropy = 0
+    for value in set(data_set[attr]):
+        subset = data_set[data_set[attr] == value].copy()
+        value_weight = len(subset) / len(data_set)
+        entropy += value_weight * get_entropy(subset[target_col])
+
+    return round(entropy, 6)
