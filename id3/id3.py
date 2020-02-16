9ab2c68a85923d00aa34a3af67ea425643293669
Skobnikoff <arthur.skobnikov@gmail.com>
2020-02-16 21:23:06 +0100

    feat(id3): remove get_information_gain method draft

--- id3/id3.py
+++ id3/id3.py
@@ -19,16 +19,6 @@ def get_entropy(target):
 
     return round(entropy, 6)
 
-def get_information_gain(data_set, attribute):
-    """
-    Calculate and return the information gain obtained by splitting the data set on the given attribute.
-    Information gain = (entropy (information richness) of the given data set) - (total sum of entropies of all subsets
-    obtained by splitting the given data set on the given attribute).
-    :return:
-    """
-    #TODO
-    return
-
 
 def get_post_split_entropy(data_set, attr, target_col='class'):
     """
