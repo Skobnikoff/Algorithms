ba522d26355e1d97a4657e3ef6d81b117ba0c665
Skobnikoff <arthur.skobnikov@gmail.com>
2020-02-16 21:53:56 +0100

    feat(id3): get best split

--- id3/id3.py
+++ id3/id3.py
@@ -38,3 +38,30 @@ def get_post_split_entropy(data_set, attr, target_col='class'):
         entropy += value_weight * get_entropy(subset[target_col])
 
     return round(entropy, 6)
+
+
+def get_best_split(data_set, unused_attributes, target_col='class'):
+
+    best_information_gain = None
+    selected_attr = None
+
+    pre_split_entropy = get_entropy(data_set[target_col])
+    print("\n> pre_split_entropy:", pre_split_entropy)
+
+    for attr in unused_attributes:
+        print("\n>", attr)
+
+        post_split_entropy = get_post_split_entropy(data_set, attr)
+        print(">> post_split_entropy:", post_split_entropy)
+
+        information_gain = round(pre_split_entropy - post_split_entropy, 6)
+        print(">> information_gain:", information_gain)
+
+        if best_information_gain is None or information_gain > best_information_gain:
+            best_information_gain = information_gain
+            selected_attr = attr
+
+    print("\n> best_information_gain:", best_information_gain)
+    print("> selected_attr:", selected_attr)
+
+    return selected_attr
