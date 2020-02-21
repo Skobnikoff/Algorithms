4774c36fe51df5b9536b3d63a5516242e301b0ab
Skobnikoff <arthur.skobnikov@gmail.com>
2020-02-21 21:34:41 +0100

    ref(id3): remove prints

--- id3/id3.py
+++ id3/id3.py
@@ -48,23 +48,14 @@ def get_best_split(data_set, unused_attributes, target_col='class'):
     selected_attr = None
 
     pre_split_entropy = get_entropy(data_set[target_col])
-    print("\n> pre_split_entropy:", pre_split_entropy)
 
     for attr in unused_attributes:
-        print("\n>", attr)
-
         post_split_entropy = get_post_split_entropy(data_set, attr)
-        print(">> post_split_entropy:", post_split_entropy)
-
         information_gain = round(pre_split_entropy - post_split_entropy, 6)
-        print(">> information_gain:", information_gain)
 
         if best_information_gain is None or information_gain > best_information_gain:
             best_information_gain = information_gain
             selected_attr = attr
-
-    print("\n> best_information_gain:", best_information_gain)
-    print("> selected_attr:", selected_attr)
 
     return selected_attr
 
@@ -107,7 +98,6 @@ def run_id3_algorithm(
     node.branches = {}
 
     for value in unique_attr_values[node.attr]:
-        print(value)
         if value not in data_set[node.attr].values:
             node.branches[value] = Node(klass=major_class)
         else:
